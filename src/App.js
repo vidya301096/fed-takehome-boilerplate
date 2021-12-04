f08f6fc134dd697fab59fe8007d08f62af4be337
Vidyasree Natarajan <vidya301096@gmail.com>
2021-12-04 12:48:38 -0800

    Molecules | Add Header
    Signed-off-by: Vidyasree Natarajan <vidya301096@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,37 +3,48 @@ import Button from './components/atoms/Button';
 import Container from './components/atoms/Container';
 import Tile from './components/atoms/Tile';
 import Logo from './assets/logo.svg';
+import Header from './components/molecules/Header';
 
 function App() {
 	return (
-		<Container>
-			<div
-				style={{
-					background: '#ebebeb',
-					width: '100%',
-					height: '200px',
-					display: 'flex',
-					justifyContent: 'center',
-					alignItems: 'center',
+		<>
+			<Header
+				links={{
+					portfolio: '/portfolio',
+					process: '/process',
+					journal: '/journal',
+					'contact info': '/contact',
 				}}
-			>
-				<Button>DOWNLOAD NOW</Button>
-			</div>
-			<Button href='https://www.google.com'>DOWNLOAD NOW</Button>
-			<Tile src={Logo} title='Article 1' date='Aug 12, 2020'>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry's standard dummy
-				text ever since the 1500s, when an unknown printer took a galley
-				of type and scrambled it to make a type specimen book. It has
-				survived not only five centuries, but also the leap into
-				electronic typesetting, remaining essentially unchanged. It was
-				popularised in the 1960s with the release of Letraset sheets
-				containing Lorem Ipsum passages, and more recently with desktop
-				publishing software like Aldus PageMaker including versions of
-				Lorem Ipsum.
-			</Tile>
-			<Tile />
-		</Container>
+			/>
+			<Container>
+				<div
+					style={{
+						background: '#ebebeb',
+						width: '100%',
+						height: '200px',
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				>
+					<Button>DOWNLOAD NOW</Button>
+				</div>
+				<Button href='https://www.google.com'>DOWNLOAD NOW</Button>
+				<Tile src={Logo} title='Article 1' date='Aug 12, 2020'>
+					Lorem Ipsum is simply dummy text of the printing and
+					typesetting industry. Lorem Ipsum has been the industry's
+					standard dummy text ever since the 1500s, when an unknown
+					printer took a galley of type and scrambled it to make a
+					type specimen book. It has survived not only five centuries,
+					but also the leap into electronic typesetting, remaining
+					essentially unchanged. It was popularised in the 1960s with
+					the release of Letraset sheets containing Lorem Ipsum
+					passages, and more recently with desktop publishing software
+					like Aldus PageMaker including versions of Lorem Ipsum.
+				</Tile>
+				<Tile />
+			</Container>
+		</>
 	);
 }
 
---
+++ src/components/molecules/Header/Header.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { object, string } from 'prop-types';
+import Container from '../../atoms/Container';
+import Button from '../../atoms/Button';
+import Logo from '../../../assets/logo.svg';
+import './Header.scss';
+
+const Header = ({ className, links, ...rest }) => {
+	return (
+		<Container
+			as='header'
+			fluid
+			className={`Header ${className}`}
+			{...rest}
+		>
+			<a href='/' className='header--logo'>
+				<img alt='Header logo' src={Logo} />
+			</a>
+			<nav className='header--link_group'>
+				{Object.keys(links).length > 0 &&
+					Object.keys(links).map((link) => {
+						return (
+							<Button href={links[link]}>
+								{link.toUpperCase()}
+							</Button>
+						);
+					})}
+			</nav>
+		</Container>
+	);
+};
+
+export default Header;
+
+Header.propTypes = {
+	className: string,
+	links: object,
+};
+
+Header.defaultProps = {
+	className: '',
+	links: {},
+};
