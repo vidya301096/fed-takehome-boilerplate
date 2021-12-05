113c64abc0f7d671afff594cc8a40c18f8e953d3
Vidyasree Natarajan <vidya301096@gmail.com>
2021-12-05 14:04:36 -0800

    Update template content
    Signed-off-by: Vidyasree Natarajan <vidya301096@gmail.com>

--- src/content/homepage.js
+++ src/content/homepage.js
@@ -22,16 +22,16 @@ const content = {
 				"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a",
 		},
 		{
-			title: 'Article 1',
-			date: 'Aug 1, 2020',
+			title: 'Article 2',
+			date: 'Aug 21, 2020',
 			href: '/article2',
 			src: '',
 			content:
 				"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a",
 		},
 		{
-			title: 'Article 1',
-			date: 'Aug 1, 2020',
+			title: 'Article 3',
+			date: 'Aug 11, 2020',
 			href: '/article3',
 			src: '',
 			content:
